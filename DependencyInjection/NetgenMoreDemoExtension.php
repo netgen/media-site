a249f51eef572b98337a65ffb041697836c4ee31
Edi Modrić <edi.modric@gmail.com>
2014-10-11 00:54:38 +0200

    Revert "Import bundle settings implicitly"
    This reverts commit f00da8fedf73d45839f2b3ef3994404f776e05d8.

--- DependencyInjection/NetgenMoreDemoExtension.php
+++ DependencyInjection/NetgenMoreDemoExtension.php
@@ -2,15 +2,12 @@
 
 namespace Netgen\Bundle\MoreDemoBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\Resource\FileResource;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
-class NetgenMoreDemoExtension extends Extension implements PrependExtensionInterface
+class NetgenMoreDemoExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -22,25 +19,5 @@ class NetgenMoreDemoExtension extends Extension implements PrependExtensionInter
 
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
         $loader->load( 'services.yml' );
-    }
-
-    /**
-     * Allow an extension to prepend the extension configurations
-     *
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
-     */
-    public function prepend( ContainerBuilder $container )
-    {
-        $configFiles = array(
-            __DIR__ . '/../Resources/config/override.yml',
-            __DIR__ . '/../Resources/config/ezpage.yml'
-        );
-
-        foreach ( $configFiles as $configFile )
-        {
-            $config = Yaml::parse( file_get_contents( $configFile ) );
-            $container->prependExtensionConfig( 'ezpublish', $config );
-            $container->addResource( new FileResource( $configFile ) );
-        }
     }
 }
