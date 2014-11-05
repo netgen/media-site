f29d3d2365cadd7346f3781bac6f6782dc2568ed
Edi Modrić <edi.modric@gmail.com>
2014-11-05 13:49:58 +0100

    Remove ezcomments from legacy site.ini

--- src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/root_dev/settings/override/site.ini.append.php
+++ src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/root_dev/settings/override/site.ini.append.php
@@ -38,7 +38,6 @@ ActiveExtensions[]=ezgmaplocation
 ActiveExtensions[]=ezdemo
 ActiveExtensions[]=ezwt
 ActiveExtensions[]=ezflow
-ActiveExtensions[]=ezcomments
 ActiveExtensions[]=ezie
 ActiveExtensions[]=ezoe
 ActiveExtensions[]=ezodf
