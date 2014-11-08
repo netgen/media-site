6c48dc89a2a69d64ac064e167120da124ca258af
Edi Modrić <edi.modric@gmail.com>
2014-11-08 23:50:52 +0100

    Unify image.ini & image.yml

--- src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/siteaccess/eng/image.ini.append.php
+++ src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/siteaccess/eng/image.ini.append.php
@@ -8,17 +8,17 @@ Filters[]=strip=-strip
 [AliasSettings]
 AliasList[]
 
-# Netgen specific, ne smiju se vidjeti u administraciji
-AliasList[]=ng_image_embed
-AliasList[]=ng_image_line
+# Netgen specific, should not appear in administration
+
 AliasList[]=ng_image_full
+AliasList[]=ng_image_line
+AliasList[]=ng_image_embed
 AliasList[]=ng_image_block_item
 AliasList[]=gallerylarge
 AliasList[]=galleryfull
 AliasList[]=gallerythumbnail
-AliasList[]=medium
 
-# ngresponsiveimage aliases
+# ngresponsiveimages aliases
 
 AliasList[]=imagefull
 AliasList[]=i320
@@ -27,75 +27,74 @@ AliasList[]=i770
 AliasList[]=i960
 AliasList[]=i1540
 
-[ng_responsive_full]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=1000
+# Used as a preview in eZ Online Editor
 
-[ng_responsive_half]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=500
-
-[imagefull]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=1000
-
-[i320]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=320
-
-[i480]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=480
-
-[i770]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=770
-
-[i960]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=960
-
-[i1540]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=1540
-
-# end ngresponsiveimage aliases
-
+AliasList[]=medium
 
 [ng_image_full]
-Reference=
+Reference=original
 Filters[]
 Filters[]=geometry/scalewidthdownonly=1170
 
 [ng_image_line]
-Reference=
+Reference=original
 Filters[]
 Filters[]=geometry/scalewidthdownonly=330
 
 [ng_image_embed]
-Reference=
+Reference=original
 Filters[]
 Filters[]=geometry/scalewidthdownonly=330
 
 [ng_image_block_item]
-Reference=
+Reference=original
 Filters[]
 Filters[]=geometry/scalewidthdownonly=330
 
 [gallerylarge]
-Reference=
+Reference=original
 Filters[]
 Filters[]=geometry/scalewidth=770
 
 [galleryfull]
-Reference=
+Reference=original
 Filters[]
 Filters[]=geometry/scalewidthdownonly=1800
 
 [gallerythumbnail]
-Reference=
+Reference=original
 Filters[]
 Filters[]=thumb=182
 Filters[]=centerimg=96;72
 Filters[]=strip=
+
+[imagefull]
+Reference=original
+Filters[]
+Filters[]=geometry/scalewidthdownonly=1000
+
+[i320]
+Reference=original
+Filters[]
+Filters[]=geometry/scalewidthdownonly=320
+
+[i480]
+Reference=original
+Filters[]
+Filters[]=geometry/scalewidthdownonly=480
+
+[i770]
+Reference=original
+Filters[]
+Filters[]=geometry/scalewidthdownonly=770
+
+[i960]
+Reference=original
+Filters[]
+Filters[]=geometry/scalewidthdownonly=960
+
+[i1540]
+Reference=original
+Filters[]
+Filters[]=geometry/scalewidthdownonly=1540
 */ ?>
