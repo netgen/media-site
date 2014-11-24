66ba0fc5dcfe4611fb8de095e9aa1bf9531c3821
Edi Modrić <edi.modric@gmail.com>
2014-11-24 13:54:56 +0100

    Add embed/line templates for image/file

--- src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/_skeleton_siteaccess/override.ini.append.php
+++ src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/_skeleton_siteaccess/override.ini.append.php
@@ -1,13 +1,5 @@
 <?php /* #?ini charset="utf-8"?
 
-# DOWNLOAD LINK
-
-[download_link]
-Source=content/datatype/view/ezxmltags/link.tpl
-MatchFile=download_link.tpl
-Subdir=templates
-Match[classification]=download_link
-
 # NG FEEDBACK FORM
 
 [full_ng_feedback_form]
@@ -28,6 +20,8 @@ MatchFile=collectedinfo/ng_feedback_form.tpl
 Subdir=templates
 Match[class_identifier]=ng_feedback_form
 
+# BLOCK
+
 [block_menu_dynamic]
 Source=block/view/view.tpl
 MatchFile=block/menu_dynamic.tpl
@@ -39,82 +33,6 @@ Source=block/view/view.tpl
 MatchFile=block/tags_cloud.tpl
 Subdir=templates
 Match[type]=TagsCloud
-
-# EZ SYSTEMS DEFAULT OVERRIDES
-
-## FILE
-
-[line_file]
-Source=node/view/line.tpl
-MatchFile=line/file.tpl
-Subdir=templates
-Match[class_identifier]=file
-
-[embed_file]
-Source=content/view/embed.tpl
-MatchFile=embed/file.tpl
-Subdir=templates
-Match[class_identifier]=file
-
-[edit_file]
-Source=content/edit.tpl
-MatchFile=edit/file.tpl
-Subdir=templates
-Match[class_identifier]=file
-
-## IMAGE
-
-[line_image]
-Source=node/view/line.tpl
-MatchFile=line/image.tpl
-Subdir=templates
-Match[class_identifier]=image
-
-[embed_image]
-Source=content/view/embed.tpl
-MatchFile=embed/image.tpl
-Subdir=templates
-Match[class_identifier]=image
-
-[embed_inline_image]
-Source=content/view/embed-inline.tpl
-MatchFile=embed-inline/image.tpl
-Subdir=templates
-Match[class_identifier]=image
-
-## LINK
-
-[line_link]
-Source=node/view/line.tpl
-MatchFile=line/link.tpl
-Subdir=templates
-Match[class_identifier]=link
-
-# DATATYPES
-
-[ezgmaplocation_article]
-Source=content/datatype/view/ezgmaplocation.tpl
-MatchFile=datatype/view/ezgmaplocation_article.tpl
-Subdir=templates
-Match[class_identifier]=article
-
-[ezstring_feedback_form]
-Source=content/datatype/collect/ezstring.tpl
-MatchFile=datatype/collect/ezstring_feedback_form.tpl
-Subdir=templates
-Match[class_identifier]=feedback_form
-
-[ezemail_feedback_form]
-Source=content/datatype/collect/ezemail.tpl
-MatchFile=datatype/collect/ezemail_feedback_form.tpl
-Subdir=templates
-Match[class_identifier]=feedback_form
-
-[eztext_feedback_form]
-Source=content/datatype/collect/eztext.tpl
-MatchFile=datatype/collect/eztext_feedback_form.tpl
-Subdir=templates
-Match[class_identifier]=feedback_form
 
 # EZXML TAGS
 
