7a4514c6024f6a3e5ac5945508270887b3484e7a
Edi Modrić <edi.modric@gmail.com>
2014-12-16 16:32:08 +0100

    Use pagelayout resolver to define which layout to use in full views

--- src/Netgen/Bundle/MoreDemoBundle/DependencyInjection/NetgenMoreDemoExtension.php
+++ src/Netgen/Bundle/MoreDemoBundle/DependencyInjection/NetgenMoreDemoExtension.php
@@ -19,6 +19,7 @@ class NetgenMoreDemoExtension extends Extension
         $config = $this->processConfiguration( $configuration, $configs );
 
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+        $loader->load( 'parameters.yml' );
         $loader->load( 'services.yml' );
         $loader->load( 'legacy.yml' );
 
