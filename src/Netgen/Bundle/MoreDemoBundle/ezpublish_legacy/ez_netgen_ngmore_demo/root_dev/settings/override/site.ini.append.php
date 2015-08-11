7574f4fc098c611c178741f30b6c81d319abf0c5
Edi Modrić <edi.modric@gmail.com>
2015-08-11 10:43:51 +0200

    Disable injection of legacy extensions by eZ kernel, it seems it kills the extension ordering defined in INI file (add ngsymfonytools to ini as it was injected before)

--- src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/root_dev/settings/override/site.ini.append.php
+++ src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/root_dev/settings/override/site.ini.append.php
@@ -15,6 +15,7 @@ VarDir=var/ezdemo_site
 
 [ExtensionSettings]
 ActiveExtensions[]=ez_netgen_ngmore_demo
+ActiveExtensions[]=ngsymfonytools
 ActiveExtensions[]=ngresponsiveimages
 ActiveExtensions[]=ngmore
 ActiveExtensions[]=ezfind
