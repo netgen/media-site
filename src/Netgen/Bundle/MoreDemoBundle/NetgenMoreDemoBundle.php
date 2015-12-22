b7a264c6c000959e4e37bd441a8323eeac991aa7
Edi Modrić <edi.modric@gmail.com>
2015-12-22 15:45:49 +0100

    Remove XSL for custom tags, moved to ngmore bundle

--- src/Netgen/Bundle/MoreDemoBundle/NetgenMoreDemoBundle.php
+++ src/Netgen/Bundle/MoreDemoBundle/NetgenMoreDemoBundle.php
@@ -3,20 +3,8 @@
 namespace Netgen\Bundle\MoreDemoBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Netgen\Bundle\MoreBundle\NetgenMoreProjectBundleInterface;
-use Netgen\Bundle\MoreDemoBundle\DependencyInjection\Compiler\XslRegisterPass;
 
 class NetgenMoreDemoBundle extends Bundle implements NetgenMoreProjectBundleInterface
 {
-    /**
-     * Builds the bundle.
-     *
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new XslRegisterPass());
-    }
 }
