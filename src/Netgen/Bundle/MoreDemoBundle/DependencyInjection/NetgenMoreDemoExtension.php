f81de700b904d3155c61a932e2f5da65fdc9cb66
Edi Modrić <edi.modric@gmail.com>
2015-12-23 13:02:33 +0100

    Load parameters.yml explicitly in ezpublish.yml

--- src/Netgen/Bundle/MoreDemoBundle/DependencyInjection/NetgenMoreDemoExtension.php
+++ src/Netgen/Bundle/MoreDemoBundle/DependencyInjection/NetgenMoreDemoExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use DirectoryIterator;
 
 class NetgenMoreDemoExtension extends Extension
 {
@@ -19,17 +18,6 @@ class NetgenMoreDemoExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('parameters.yml');
-        $loader->load('templates.yml');
         $loader->load('services.yml');
-        $loader->load('legacy.yml');
-
-        foreach (new DirectoryIterator(__DIR__ . '/../Resources/config/legacy') as $legacyConfig) {
-            if ($legacyConfig->isDir() || $legacyConfig->isDot()) {
-                continue;
-            }
-
-            $loader->load('legacy/' . $legacyConfig->getBasename());
-        }
     }
 }
