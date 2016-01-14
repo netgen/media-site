7b433ec9541b82119b0925b21330dc5feea50979
Edi Modrić <edi.modric@gmail.com>
2016-01-14 11:16:32 +0100

    Remove legacy google maps settings from ngmore.ini

--- src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/root_prod/settings/siteaccess/eng/ngmore.ini.append.php
+++ src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/root_prod/settings/siteaccess/eng/ngmore.ini.append.php
@@ -1,12 +1,5 @@
 <?php /* #?ini charset="utf-8"?
 
-[GoogleMapsSettings]
-DefaultLatitude=45.79849
-DefaultLongitude=15.90777
-DefaultZoom=13
-# Valid options are ROADMAP, SATELLITE, HYBRID, TERRAIN
-DefaultMapType=ROADMAP
-
 [SpecialNodes]
 SidebarsParentNode=83
 */ ?>
