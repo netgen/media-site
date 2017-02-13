a9ef2c8277dacc53e9971ae13129b1964cf88fd0
Edi Modrić <edi.modric@gmail.com>
2017-02-13 12:09:34 +0100

    Implement info collection

--- src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/_skeleton_siteaccess/override.ini.append.php
+++ src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/_skeleton_siteaccess/override.ini.append.php
@@ -1,25 +1,5 @@
 <?php /* #?ini charset="utf-8"?
 
-# NG FEEDBACK FORM
-
-[full_ng_feedback_form]
-Source=node/view/full.tpl
-MatchFile=full/ng_feedback_form.tpl
-Subdir=templates
-Match[class_identifier]=ng_feedback_form
-
-[ng_feedback_form_collectedinfomail]
-Source=content/collectedinfomail/form.tpl
-MatchFile=collectedinfomail/ng_feedback_form.tpl
-Subdir=templates
-Match[class_identifier]=ng_feedback_form
-
-[ng_feedback_form_collectedinfo]
-Source=content/collectedinfo/form.tpl
-MatchFile=collectedinfo/ng_feedback_form.tpl
-Subdir=templates
-Match[class_identifier]=ng_feedback_form
-
 ## DEFAULT OVERRIDE FOR CLASSES WITHOUT FULL TEMPLATES
 
 [not_overriden_content]
