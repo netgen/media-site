e5a4d5671d5a1926525a8aeb33ab169880dc5ee3
Edi Modrić <edi.modric@gmail.com>
2018-03-01 10:52:35 +0100

    CS fixes

--- src/Netgen/Bundle/MoreDemoBundle/DependencyInjection/NetgenMoreDemoExtension.php
+++ src/Netgen/Bundle/MoreDemoBundle/DependencyInjection/NetgenMoreDemoExtension.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\MoreDemoBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class NetgenMoreDemoExtension extends Extension
 {
--- src/Netgen/Bundle/MoreDemoBundle/NetgenMoreDemoBundle.php
+++ src/Netgen/Bundle/MoreDemoBundle/NetgenMoreDemoBundle.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\MoreDemoBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Netgen\Bundle\MoreBundle\NetgenMoreProjectBundleInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NetgenMoreDemoBundle extends Bundle implements NetgenMoreProjectBundleInterface
 {
