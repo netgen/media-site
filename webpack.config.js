4a09594f5863094b3f7554f9752db6edc96559da
Edi Modrić <edi.modric@gmail.com>
2018-03-12 14:58:17 +0100

    Typos

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 // webpack.config.js
 const config = require('./webpack.config.default');
 
-// for multiple webpack configurations create another webpack config file and require it here
+// for multiple webpack configurations, create another webpack config file and require it here
 // const anotherConfig = require('./webpack.config.anotherConfig');
 
 // export the final configuration
-// for multiple webpack configuration add imported configuration to export array eg. module.exports = [config, anotherConfig];
+// for multiple webpack configurations, add imported configuration to export array eg. module.exports = [config, anotherConfig];
 module.exports = [config];
