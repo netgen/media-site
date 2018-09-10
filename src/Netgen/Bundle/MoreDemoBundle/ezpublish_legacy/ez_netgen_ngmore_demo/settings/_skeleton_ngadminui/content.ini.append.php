80c573cc75611628ccb8d40646fff4a083fb5f61
Goran Martinjak <goran@netgen.hr>
2018-09-10 16:41:14 +0200

    BASESITE-206 - add default and highlighted button classes to xml editor

--- src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/_skeleton_ngadminui/content.ini.append.php
+++ src/Netgen/Bundle/MoreDemoBundle/ezpublish_legacy/ez_netgen_ngmore_demo/settings/_skeleton_ngadminui/content.ini.append.php
@@ -1,5 +1,11 @@
 <?php /* #?ini charset="utf-8"?
 
+[link]
+AvailableClasses[]=btn btn-default
+AvailableClasses[]=btn btn-primary
+ClassDescription[btn btn-default]=Default button
+ClassDescription[btn btn-primary]=Highlighted button
+
 [VersionView]
 AvailableSiteDesignList[]
 AvailableSiteDesignList[]=admin2
