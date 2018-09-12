00d7b1c22ba4b6f4d42f67f4b339791be2aea276
Edi Modrić <edi.modric@gmail.com>
2018-09-12 14:50:02 +0200

    Remove obsolete legacy siteaccess configuration, simplify generation of legacy and ngadminui siteaccess config

--- src/Netgen/Bundle/SiteDemoBundle/ezpublish_legacy/ngsite_demo/settings/_skeleton_ngadminui/dashboard.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[DashboardSettings]
-DashboardBlocks[]
-DashboardBlocks[]=pending_list
-DashboardBlocks[]=drafts
-DashboardBlocks[]=latest_content
-DashboardBlocks[]=all_latest_content
-*/ ?>
