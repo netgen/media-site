02ddf062c9f899357c8017ff09c48151746ba13b
Goran Martinjak <goran@netgen.hr>
2019-03-21 11:12:18 +0100

    babel configure useBuiltIns fix

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = ['@babel/preset-env'];
+  const presets = [
+    ['@babel/preset-env', {
+      useBuiltIns: 'entry',
+      corejs: 3,
+    }],
+  ];
 
   return {
     presets,
