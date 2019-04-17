bbb39b573daedd6f12bdf0b1c79a54c36e9d3ed3
Edi Modrić <edi.modric@gmail.com>
2019-04-17 22:11:16 +0200

    Update compile_assets.sh

--- bin/compile_assets.sh
+++ bin/compile_assets.sh
@@ -3,7 +3,7 @@
 # Script to generate assets using `yarn encore`.
 # It checks SYMFONY_ENV to ensure assets are generated for correct production/development environment.
 
-# NOTE: Check for Symfony environment is commented out since we do not need to build eZ Platform UI
+# NOTE: Check for Symfony environment is commented out since we do not need to build eZ Platform Admin UI
 # assets in Symfony dev environment. Uncomment the check if you plan to add building assets to composer.json
 # as provided by eZ Platform default install
 
