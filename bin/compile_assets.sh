b543c1d8dd2ccca218ffd5e8d44d448920e75344
Edi Modrić <edi.modric@gmail.com>
2019-04-17 22:09:20 +0200

    We don't need dev assets for eZ Platform Admin UI

--- bin/compile_assets.sh
+++ bin/compile_assets.sh
@@ -3,8 +3,12 @@
 # Script to generate assets using `yarn encore`.
 # It checks SYMFONY_ENV to ensure assets are generated for correct production/development environment.
 
-if [ "${SYMFONY_ENV}" == "dev" ] ; then
-    yarn encore dev --config=webpack.config.ezplatform.js
-else
+# NOTE: Check for Symfony environment is commented out since we do not need to build eZ Platform UI
+# assets in Symfony dev environment. Uncomment the check if you plan to add building assets to composer.json
+# as provided by eZ Platform default install
+
+# if [ "${SYMFONY_ENV}" == "dev" ] ; then
+#     yarn encore dev --config=webpack.config.ezplatform.js
+# else
     yarn encore prod --config=webpack.config.ezplatform.js
-fi
+# fi
