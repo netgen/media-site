0ff72c8704121d539a1279e67f7418e40e510ea4
Edi Modrić <edi.modric@gmail.com>
2021-03-16 10:20:14 +0100

    Remove unused default eZ app build

--- webpack.config.ezplatform.js
+++ webpack.config.ezplatform.js
@@ -5,44 +5,4 @@ const eZConfigManager = require('./ez.webpack.config.manager.js');
 const eZConfig = getEzConfig(Encore);
 const customConfigs = require('./ez.webpack.custom.configs.js');
 
-Encore.reset();
-Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-//    .enableStimulusBridge('./assets/controllers.json')
-    .enableSassLoader()
-    .enableReactPreset()
-    .enableSingleRuntimeChunk()
-//    .copyFiles({
-//        from: './assets/images',
-//        to: 'images/[path][name].[ext]',
-//        pattern: /\.(png|svg)$/
-//    })
-    .configureBabel((config) => {
-        config.plugins.push('@babel/plugin-proposal-class-properties');
-    })
-
-    // enables @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
-    })
-;
-
-// Welcome page stylesheets
-// Encore.addEntry('welcome_page', [
-//     path.resolve(__dirname, './assets/scss/welcome-page.scss'),
-// ]);
-
-// Encore.addEntry('app', './assets/app.js');
-
-// 'bootstrap' is added as a dummy entry to prevent crashing the build
-Encore.addEntry('app_dummy', [
-    'bootstrap'
-]);
-
-const projectConfig = Encore.getWebpackConfig();
-module.exports = [ eZConfig, ...customConfigs, projectConfig ];
-
-// uncomment this line if you've commented-out the above lines
-// module.exports = [ eZConfig, ...customConfigs ];
+module.exports = [ eZConfig, ...customConfigs ];
