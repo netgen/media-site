cd352a4e11efa11301759ee40a7de24b25aa749e
Deployer <deployer@netgen.io>
2021-07-05 13:15:26 +0200

    NGSTACK-521 create tag on git repository when deploying to prod

---
+++ deploy/tasks/git.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Deployer;
+
+desc('Create a tag on git repository');
+task('git:tag:add', function () {
+    $git = get('bin/git');
+    $host = get('hostname');
+    $branch = get('branch', 'unknown');
+    $date = new \DateTimeImmutable();
+
+    $tag = $date->format('Y.m.d.H.i');
+    $message = 'Deployed branch ' . $branch . ' to ' . $host;
+
+    cd('{{release_path}}');
+
+    $commitHash = trim(run('git rev-parse HEAD'));
+
+    runLocally("$git tag -a $tag -m '" . $message . "' " . $commitHash);
+    runLocally("$git push origin $tag");
+})->onStage('prod');
