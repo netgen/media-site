596cc0cc66324a1a69a3b56eb9c741bcca6b381a
Edi Modrić <edi.modric@gmail.com>
2022-02-28 12:20:26 +0100

    EzFormsBundle is now IbexaFormsBundle

--- config/bundles.php
+++ config/bundles.php
@@ -75,7 +75,7 @@ return [
     Netgen\Bundle\SiteInstallerBundle\NetgenSiteInstallerBundle::class => ['all' => true],
     Netgen\Bundle\SiteBundle\NetgenSiteBundle::class => ['all' => true],
     // Netgen\Bundle\EzPlatformSearchExtraBundle\NetgenEzPlatformSearchExtraBundle::class => ['all' => true],
-    Netgen\Bundle\EzFormsBundle\NetgenEzFormsBundle::class => ['all' => true],
     Netgen\Bundle\InformationCollectionBundle\NetgenInformationCollectionBundle::class => ['all' => true],
     Netgen\Bundle\IbexaSiteApiBundle\NetgenIbexaSiteApiBundle::class => ['all' => true],
+    Netgen\Bundle\IbexaFormsBundle\NetgenIbexaFormsBundle::class => ['all' => true],
 ];
