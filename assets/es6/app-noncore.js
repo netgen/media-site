02745f97699456761cda01b823c9e416245cb97b
Josip Sušić <josip@netgen.io>
2022-06-30 13:30:54 +0200

    NGGW-55 Extract Lazy Loading into the component

--- assets/es6/app-noncore.js
+++ assets/es6/app-noncore.js
@@ -3,7 +3,33 @@
 // exchanging variable_name with the global variable
 // /* global <variable_name> */
 import $ from 'jquery';
+<<<<<<< HEAD:assets/es6/app-noncore.js
 import '../sass/style-noncore.scss';
+=======
+import './ngsite';
+import '../sass/style.scss';
+import LazyLoading from './components/LazyLoading.component';
+
+const components = [
+  {
+    class: LazyLoading,
+    options: {
+      triggerElement: 'img'
+    },
+    selector: 'html'
+  },
+];
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  components.forEach((component) => {
+    if (document.querySelector(component.selector) !== null) {
+      document.querySelectorAll(component.selector).forEach(
+        element => new component.class(element, component.options)
+      );
+    }
+  });
+});
+>>>>>>> NGGW-55 Extract Lazy Loading into the component:src/AppBundle/Resources/es6/app.js
 
 $(() => {
   // Put your code here
