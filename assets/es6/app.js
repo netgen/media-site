2d0199e37d990f3c70f5e7b3079ecb1a9e8ae2a0
Marko Lukac <marko@netgen.io>
2022-10-24 16:07:45 +0200

    NGSTACK-608 - add video handling to components

--- assets/es6/app.js
+++ assets/es6/app.js
@@ -5,9 +5,26 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import $ from 'jquery';
+import VideoHandlerComponent from './components/VideoHandler.component';
 import './ngsite';
 import '../sass/style.scss';
 
+const components = [
+  {
+    class: VideoHandlerComponent,
+    selector: '.js-responsive-video',
+  },
+];
+
 $(() => {
-  // Put your code here
+
+  components.forEach((component) => {
+    if (document.querySelector(component.selector) !== null) {
+      document.querySelectorAll(component.selector).forEach(
+        element => new component.class(element, component.options)
+      );
+    }
+  });
+
+  // Put your code inside ./components
 });
---
+++ assets/es6/components/VideoHandler.component.js
@@ -0,0 +1,47 @@
+// import $ from 'jquery';
+
+export default class VideoHandler {
+    constructor(element) {
+        this.element = element;
+        this.onInit();
+    }
+
+    onInit() {
+
+        //add source to video tag
+        function addSourceToVideo(element, src) {
+            var source = document.createElement('source');
+            source.src = src;
+            source.type = 'video/mp4';
+            element.appendChild(source);
+        }
+
+        //determine screen size and init mobile or desktop video
+        function whichSizeVideo(element, src) {
+            var windowWidth = window.innerWidth ? window.innerWidth : $(window).width();
+            if (windowWidth >= 768) {
+                if(src.hasAttribute('data-desktop-vid'))
+                    addSourceToVideo( element, src.dataset.desktopVid);
+            } else {
+                if(src.hasAttribute('data-mobile-vid'))
+                    addSourceToVideo(element, src.dataset.mobileVid);
+            }
+        }
+
+        //init only if page has videos
+        function videoSize() {
+            //get all videos
+            var video = document.querySelectorAll('video.js-responsive-video')
+            if (video !== undefined) {
+            video.forEach(function(element, index) {
+                whichSizeVideo(
+                    element, //element
+                    element  //src locations
+                );
+            });
+            }
+        }
+        videoSize();
+
+    }
+}
