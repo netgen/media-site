d54455f7d6226cd83a80975ddf9abc28f2c5413f
Marko Sandalj <marko.s@netgen.io>
2022-11-14 12:03:01 +0100

    NGGW-55 remove old unsed code from noncore js

--- assets/es6/app-noncore.js
+++ assets/es6/app-noncore.js
@@ -3,41 +3,8 @@
 // exchanging variable_name with the global variable
 // /* global <variable_name> */
 import $ from 'jquery';
-<<<<<<< HEAD:assets/es6/app-noncore.js
 import '../sass/style-noncore.scss';
-=======
-import './ngsite';
-import '../sass/style.scss';
-import LazyLoading from './components/LazyLoading.component';
-import CookieControlClass from './components/CookieControl.component';
 
-const components = [
-  {
-    class: LazyLoading,
-    options: {
-      triggerElement: 'img'
-    },
-    selector: 'html'
-  },
-  {
-    class: CookieControlClass,
-    options: {
-      triggerElement: 'html'
-    },
-    selector: 'html'
-  }
-];
-
-window.addEventListener('DOMContentLoaded', (e) => {
-  components.forEach((component) => {
-    if (document.querySelector(component.selector) !== null) {
-      document.querySelectorAll(component.selector).forEach(
-        element => new component.class(element, component.options)
-      );
-    }
-  });
-});
->>>>>>> NGGW-55 Extract Lazy Loading into the component:src/AppBundle/Resources/es6/app.js
 
 $(() => {
   // Put your code here
