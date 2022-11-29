c56930d8a81aa8cb022c3333ff6f01c8d878b167
Petar Španja <petar@spanja.info>
2022-11-29 10:54:59 +0100

    NGSTACK-630: implement generic modal view controller

--- src/Controller/ModalViewController.php
+++ src/Controller/ModalViewController.php
@@ -1,37 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Netgen\Bundle\IbexaSiteApiBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Netgen\Bundle\IbexaSiteApiBundle\View\ContentRenderer;
 use Symfony\Component\HttpFoundation\Response;
 
 class ModalViewController extends Controller
 {
+    private ContentRenderer $contentRenderer;
+
+    public function __construct(ContentRenderer $contentRenderer)
+    {
+        $this->contentRenderer = $contentRenderer;
+    }
+
     /**
-     * @todo use form content ID, render modal view, check ctype, rename serveModalForm, generic for modal view?
-     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException
      * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
      * @throws \Ibexa\Contracts\Core\Repository\Exceptions\UnauthorizedException
      * @throws \Netgen\IbexaSiteApi\API\Exceptions\TranslationNotMatchedException
      */
-    public function viewModal(Request $request, int $formLocationId, ?int $refererLocationId = null): Response
+    public function viewModal(int $contentId): Response
     {
-        $location = $this->getSite()->getLoadService()->loadLocation($formLocationId);
-
-        $response = new Response();
-        $response->setSharedMaxAge(0);
-        $response->setPrivate();
-
-        // todo render modal view
-        return $this->render(
-            '@ibexadesign/info_collection/modal.html.twig',
-            [
-                'content' => $location->content,
-                'location' => $location,
-                'view_type' => 'embed',
-                'referer' => $this->getReferer($refererLocationId),
-            ]
+        return new Response(
+            $this->contentRenderer->renderContent(
+                $this->getSite()->getLoadService()->loadContent($contentId),
+                'modal',
+            )
         );
     }
 }
