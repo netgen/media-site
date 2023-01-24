3a3a85244e6ac3f7c4090cb9c01476ec1870891c
amalija-ramljak <amalija@netgen.io>
2023-01-24 15:27:16 +0100

    NGSTACK-667 remove JS lazy loading

--- assets/js/components/AjaxCollection.component.js
+++ assets/js/components/AjaxCollection.component.js
@@ -1,40 +1,30 @@
-import VideoPoster from "./VideoPoster.component";
+import VideoPoster from './VideoPoster.component';
 
 export default class AjaxCollection {
-    constructor(element, options) {
-        this.el = element;
-        this.options = options;
+  constructor(element, options) {
+    this.element = element;
+    this.options = options;
 
-        this.onInit();
-    }
+    this.init();
+  }
 
-    onInit() {
-        this.el.addEventListener('ajax-paging-added', () => {
-            const posters = this.el.querySelectorAll(this.options.posters);
+  init() {
+    this.element.addEventListener(
+      'ajax-paging-added',
+      this.handleAjaxPagingAddedEvent.bind(this),
+      false
+    );
+  }
 
-            posters.forEach(poster => {
-                new VideoPoster(poster, {
-                    vimeoClass: this.options.vimeoClass,
-                    dailymotionClass: this.options.dailymotionClass,
-                    posterLinkElement: this.options.posterLinkElement
-                })
-            })
+  handleAjaxPagingAddedEvent() {
+    const posters = this.element.querySelectorAll(this.options.posters);
 
-            this.el.querySelectorAll('img[data-src]').forEach((img) => {
-                this.lazyImageLoad(img)
-            })
-        }, false)
-    }
-
-    lazyImageLoad(image) {
-        if (image.hasAttribute('data-src')) image.setAttribute('src', image.getAttribute('data-src'));
-        if (image.hasAttribute('data-srcset')) {
-            image.setAttribute('srcset', image.getAttribute('data-srcset'));
-        }
-
-        image.onload = () => {
-            image.removeAttribute('data-src');
-            image.removeAttribute('data-srcset');
-        };
-    }
+    posters.forEach((poster) => {
+      new VideoPoster(poster, {
+        vimeoClass: this.options.vimeoClass,
+        dailymotionClass: this.options.dailymotionClass,
+        posterLinkElement: this.options.posterLinkElement,
+      });
+    });
+  }
 }
