4a200854782a36ebe4ddea11eee1774cb8be589a
Danilo Kondić <danilo@netgen.hr>
2023-01-24 23:45:43 +0100

    NGSTACK-671 - WIP

--- assets/js/components/VideoModal.component.js
+++ assets/js/components/VideoModal.component.js
@@ -1,88 +1,140 @@
-import { Modal } from 'bootstrap';
+import BootstrapModalDynamic from '../utilities/bootstrap-modal-dynamic/bootstrap-modal-dynamic';
 
-const externalVideoTemplate = ( videoFileLink, videoMimeType, videoPoster, videoAutoplay, videoAutoResize) =>
+const videoSourceTemplate = (collectedVideoOptions) =>
+  `<source src="${collectedVideoOptions.fileLink}" type="${collectedVideoOptions.mimeType}" />`;
+const uploadVideoTemplate = (collectedVideoOptions, videoSource, autoplayAttribute) =>
   `<video
-    id="video-${videoFileLink}"
-    class="video-js ${videoAutoResize}"
-    controls
-    preload="auto"
-    width="1024"
-    ${videoAutoplay}
-    poster="${videoPoster}"
-   
-    <source
-      src="${videoFileLink}"
-      type="${videoMimeType}"
-    />
-  </video>
-
-  <link href="https://vjs.zencdn.net/7.20.3/video-js.css" rel="stylesheet" />
-  <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>`;
-const youtubeVideoTemplate = (videoIdentifier, videoAutoplay) =>
+    id="${collectedVideoOptions.id}"
+    data-setup='{}'
+    class="video-js ${collectedVideoOptions.class}"
+    ${collectedVideoOptions.controls}
+    preload="${collectedVideoOptions.preload}"
+    width="${collectedVideoOptions.width}"
+    poster="${collectedVideoOptions.poster}"
+    ${autoplayAttribute}>
+    ${videoSource}
+  </video>`;
+const youtubeVideoTemplate = (videoIdentifier, autoplayAttribute) =>
   `<div class="video-youtube ratio ratio-16x9">
-    <iframe frameborder="0" src="https://www.youtube.com/embed/${videoIdentifier} ${videoAutoplay}" width="770" height="433" allowfullscreen></iframe>
+    <iframe frameborder="0" src="https://www.youtube.com/embed/${videoIdentifier}" ${autoplayAttribute} width="770" height="433" allowfullscreen></iframe>
   </div>`;
-const vimeoVideoTemplate = (videoIdentifier, videoAutoplay) =>
+const vimeoVideoTemplate = (videoIdentifier, autoplayAttribute) =>
   `<div class="video-vimeo ratio ratio-16x9">
-    <iframe frameborder="0" src="https://player.vimeo.com/video/${videoIdentifier} ${videoAutoplay}" width="770" height="433" allowfullscreen></iframe>
+    <iframe frameborder="0" src="https://player.vimeo.com/video/${videoIdentifier}" ${autoplayAttribute} width="770" height="433" allowfullscreen></iframe>
   </div>`;
-const dailymotionVideoTemplate = (videoIdentifier, videoAutoplay) =>
+const dailymotionVideoTemplate = (videoIdentifier, autoplayAttribute) =>
   `<div class="video-dailymotion ratio ratio-16x9">
-    <iframe frameborder="0" src="https://www.dailymotion.com/embed/video/${videoIdentifier} ${videoAutoplay}" width="770" height="433" allowfullscreen></iframe>
+    <iframe frameborder="0" src="https://www.dailymotion.com/embed/video/${videoIdentifier}" ${autoplayAttribute} width="770" height="433" allowfullscreen></iframe>
   </div>`;
 
 export default class VideoModalComponent {
   constructor(trigger, options) {
     this.options = options;
     this.trigger = trigger;
-    this.modalPlaceholder = document.querySelector('#modal-placeholder')
-    this.modal = new Modal(this.modalPlaceholder);
+    this.collectedVideoOptions = null;
+    this.bootstrapModalDynamic = null;
+    this.modalContent = null;
+    this.promises = [];
+    this.externalAssets = [
+      { url: 'https://vjs.zencdn.net/7.20.3/video-js.min.css', type: 'text/css' },
+      { url: 'https://vjs.zencdn.net/7.20.3/video.min.js', type: 'text/javascript' },
+    ];
+
     this.init();
   }
 
   init() {
     this.trigger.addEventListener('click', (e) => {
       e.preventDefault();
-      const videoType = this.trigger.getAttribute('data-video-type');
-      const videoIdentifier = this.trigger.getAttribute('data-video-identifier');
-      let videoAutoResize = '';
-      const autoResize = this.trigger.getAttribute('data-video-autoresize');
-      if (autoResize === 'fit') {
-        videoAutoResize = `vjs-fluid`;
-      } else if (autoResize === 'fill') {
-        videoAutoResize = `vjs-fill`;
-      }
-      videoAutoResize = `vjs-fill`;
-      let videoAutoplay = '';
-      const autoplayBool = this.trigger.getAttribute('data-video-autoplay');
-      if (autoplayBool) {
-        videoAutoplay = 'autoplay'
-      }
-      let modalContent = null;
-      if (videoType === 'upload') {
-        const videoFileLink = this.trigger.getAttribute('data-video-file-link');
-        const videoMimeType = this.trigger.getAttribute('data-video-mime-type');
-        const videoPoster = this.trigger.getAttribute('data-video-poster');
-        modalContent = externalVideoTemplate(videoFileLink, videoMimeType, videoPoster, videoAutoplay, videoAutoResize)
-      } else if (videoType === 'youtube') {
-        modalContent = youtubeVideoTemplate(videoIdentifier, videoAutoplay)
-      } else if (videoType === 'vimeo') {
-        modalContent = vimeoVideoTemplate(videoIdentifier, videoAutoplay)
-      } else if (videoType === 'dailymotion') {
-        modalContent = dailymotionVideoTemplate(videoIdentifier, videoAutoplay)
-      }
-      this.openModal(modalContent);
+
+      // this.setupVideo();
+
+      this.externalAssets.forEach((asset) => {
+        if(document.getElementById(`video-js-${asset.type}`) === null) {
+          this.promises.push(this.loadExternalAssets(asset));
+        }
+      });
+
+      Promise.all(this.promises)
+        .then(() => {
+          console.log('all scripts loaded');
+          this.setupVideo();
+        })
+        .catch((script) => {
+          console.log(`${script} failed to load`);
+        });
     });
   }
 
-  openModal(modalContent) {
+  setupVideo() {
+    this.collectedVideoOptions = JSON.parse(this.trigger.getAttribute('data-video-options'));
+    this.collectedVideoOptions.id = `video-js-${this.collectedVideoOptions.type}`;
+    this.collectedVideoOptions.width = '100%';
+    this.collectedVideoOptions.controls = 'controls';
+    this.collectedVideoOptions.preload = 'auto';
+    if (this.collectedVideoOptions.autoResize === 'fit') {
+      this.collectedVideoOptions.class = 'vjs-fluid';
+    } else if (this.collectedVideoOptions.autoResize === 'fill') {
+      this.collectedVideoOptions.class = 'vjs-fill';
+    }
+    this.collectedVideoOptions.class = 'vjs-fill';
 
-    this.modalPlaceholder.addEventListener('hidden.bs.modal', () => this.closeModal(this.modal, this.modalPlaceholder));
-    this.modalPlaceholder.querySelector('.modal-body').innerHTML = modalContent;
-    this.modal.show();
+    let autoplayAttribute = '';
+    if (this.collectedVideoOptions.autoplay) {
+      autoplayAttribute = 'autoplay';
+    }
+
+    if (this.collectedVideoOptions.type === 'upload') {
+      this.modalContent = uploadVideoTemplate(this.collectedVideoOptions, videoSourceTemplate(this.collectedVideoOptions), autoplayAttribute);
+    } else if (this.collectedVideoOptions.type === 'youtube') {
+      this.modalContent = youtubeVideoTemplate(this.collectedVideoOptions.identifier, autoplayAttribute);
+    } else if (this.collectedVideoOptions.type === 'vimeo') {
+      this.modalContent = vimeoVideoTemplate(this.collectedVideoOptions.identifier, autoplayAttribute);
+    } else if (this.collectedVideoOptions.type === 'dailymotion') {
+      this.modalContent = dailymotionVideoTemplate(this.collectedVideoOptions.identifier, autoplayAttribute);
+    }
+
+    this.createBootstrapModalDynamic();
   }
 
-  closeModal() {
-    this.modal.hide();
+  createBootstrapModalDynamic() {
+    this.bootstrapModalDynamic = new BootstrapModalDynamic({
+      receivedContent: this.modalContent,
+      modalCssClasses: 'video-modal',
+      dialogCssClasses: 'modal-dialog-centered modal-fullscreen',
+      modalId: 'video-js-modal',
+      transition: 'fade',
+    });
+  }
+
+  loadExternalAssets() {
+    const head = document.getElementsByTagName('head')[0];
+    return new Promise((resolve, reject) => {
+      this.externalAssets.forEach((asset) => {
+        if(document.getElementById(`video-js-${asset.type}`) === null) {
+          let fileRef = null;
+          if (asset.type === 'text/javascript') {
+            fileRef = document.createElement('script');
+          } else if (asset.type === 'text/css') {
+            fileRef = document.createElement('link');
+            fileRef.setAttribute('rel', 'stylesheet');
+          }
+          fileRef.setAttribute('id', `video-js-${asset.type}`);
+          fileRef.setAttribute('type', asset.type);
+          fileRef.setAttribute('src', asset.url);
+          if (asset.type === 'text/javascript') {
+            fileRef.onload = () => {
+              resolve();
+            }
+            fileRef.onerror = () => {
+              reject(new Error(`cannot load file ${ asset.url}`));
+            }
+          } else {
+            resolve();
+          }
+          head.appendChild(fileRef);
+        }
+      });
+    })
   }
 }
---
+++ assets/js/utilities/bootstrap-modal-dynamic/bootstrap-modal-dynamic.js
@@ -0,0 +1,99 @@
+import { Modal } from 'bootstrap';
+
+const bootstrapModalTemplate = (
+  modalCssClasses,
+  dialogCssClasses,
+  modalId = 'bootstrap-modal-placeholder',
+  transition = 'fade'
+) =>
+  `<div id="${modalId}" class="modal ${transition} ${modalCssClasses}" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static">
+    <div class="modal-dialog ${dialogCssClasses}" role="document">
+      <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body"></div>
+      </div>
+    </div>
+</div>`;
+
+export default class BootstrapModalDynamic {
+  constructor({
+    receivedContent,
+    modalCssClasses,
+    dialogCssClasses,
+    modalId,
+    transition,
+    onOpenCallback,
+    onCloseCallback,
+  }) {
+    this.modalCssClasses = modalCssClasses;
+    this.dialogCssClasses = dialogCssClasses;
+    this.modalId = modalId;
+    this.transition = transition;
+    this.receivedContent = receivedContent;
+    this.onOpenCallback = onOpenCallback;
+    this.onCloseCallback = onCloseCallback;
+    this.modalShown = false;
+    this.modalElement = null;
+    this.bootstrapModal = null;
+    this.tempModal = document.createElement('tempModal');
+    this.contentElement = null;
+    this.tempContent = document.createElement('tempContent');
+
+    this.init();
+  }
+
+  init() {
+    if (this.modalShown) {
+      this.closeModal();
+    }
+    this.createModal();
+  }
+
+  createModal() {
+    this.tempModal.innerHTML = bootstrapModalTemplate(
+      this.modalCssClasses,
+      this.dialogCssClasses,
+      this.modalId,
+      this.transition
+    );
+    this.modalElement = this.tempModal.firstChild;
+    this.bootstrapModal = new Modal(this.modalElement);
+    this.setupContent();
+  }
+
+  setupContent() {
+    this.tempContent.innerHTML = this.receivedContent.trim();
+    this.contentElement = this.tempContent.firstChild;
+    this.modalElement.querySelector('.modal-body').appendChild(this.contentElement);
+    this.setupEvents();
+  }
+
+  setupEvents() {
+    this.modalElement.addEventListener('hidden.bs.modal', () => this.closeModal());
+    this.insertModalToDom();
+  }
+
+  insertModalToDom() {
+    document.body.appendChild(this.modalElement);
+    this.openModal();
+  }
+
+  openModal() {
+    this.bootstrapModal.show();
+    this.modalShown = true;
+    if (this.onOpenCallback && this.onOpenCallback instanceof Function) {
+      this.onOpenCallback();
+    }
+  }
+
+  closeModal() {
+    this.bootstrapModal.dispose();
+    this.modalElement.remove();
+    this.modalShown = false;
+    if (this.onOpenCallback && this.onOpenCallback instanceof Function) {
+      this.onCloseCallback();
+    }
+  }
+}
