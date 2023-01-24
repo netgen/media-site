076ce8dbf47590d523d65208bfb44e743148d5d3
amalija-ramljak <amalija@netgen.io>
2023-01-24 16:28:18 +0100

    NGSTACK-667 minor google maps cleanup

--- assets/js/components/GoogleMap.component.js
+++ assets/js/components/GoogleMap.component.js
@@ -1,21 +1,28 @@
 /* global google */
 
 export default class GoogleMap {
-  constructor(el) {
-    if (typeof google === 'undefined' || typeof google.maps === 'undefined') return;
+  constructor(element) {
+    if (typeof google === 'undefined' || typeof google.maps === 'undefined') {
+      console.group('Google Maps error');
+      console.info('Global "google" or "google.maps" is not defined.');
+      console.log({ google });
+      console.groupEnd();
 
-    this.el = el;
-    this.latitude = parseFloat(this.el.dataset.latitude);
-    this.longitude = parseFloat(this.el.dataset.longitude);
-    this.zoom = parseInt(this.el.dataset.zoom, 10);
-    this.mapType = this.el.dataset.mapType;
-    this.showMarker = this.el.hasAttribute('data-show-marker');
+      return;
+    }
+
+    this.element = element;
+    this.latitude = parseFloat(this.element.dataset.latitude);
+    this.longitude = parseFloat(this.element.dataset.longitude);
+    this.zoom = parseInt(this.element.dataset.zoom, 10);
+    this.mapType = this.element.dataset.mapType;
+    this.showMarker = this.element.dataset.showMarker;
 
     this.init();
   }
 
   init() {
-    this.map = new google.maps.Map(this.el, {
+    this.map = new google.maps.Map(this.element, {
       center: { lat: this.latitude, lng: this.longitude },
       zoom: this.zoom,
       mapTypeId: google.maps.MapTypeId[this.mapType],
