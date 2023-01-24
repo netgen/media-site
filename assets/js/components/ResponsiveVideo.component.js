93e52dad05ca828214c8c35b6c62a2d7614ed8e1
amalija-ramljak <amalija@netgen.io>
2023-01-24 16:46:05 +0100

    NGSTACK-667 clean up responsive video
    Handles only one responsive video instead of each handling all

--- assets/js/components/ResponsiveVideo.component.js
+++ assets/js/components/ResponsiveVideo.component.js
@@ -1,45 +1,28 @@
 export default class VideoHandler {
-    constructor(element) {
-        this.element = element;
-        this.onInit();
+  constructor(element) {
+    this.element = element;
+
+    this.init();
+  }
+
+  init() {
+    this.setSourceBasedOnWindowSize();
+  }
+
+  setSourceBasedOnWindowSize() {
+    let srcDatasetKey = 'desktopVid';
+    if (window.innerWidth < 768) {
+      srcDatasetKey = 'mobileVid';
     }
 
-    onInit() {
-
-        //add source to video tag
-        function addSourceToVideo(element, src) {
-            var source = document.createElement('source');
-            source.src = src;
-            source.type = 'video/mp4';
-            element.appendChild(source);
-        }
-
-        //determine screen size and init mobile or desktop video
-        function whichSizeVideo(element, src) {
-            var windowWidth = window.innerWidth ? window.innerWidth : $(window).width();
-            if (windowWidth >= 768) {
-                if(src.hasAttribute('data-desktop-vid'))
-                    addSourceToVideo( element, src.dataset.desktopVid);
-            } else {
-                if(src.hasAttribute('data-mobile-vid'))
-                    addSourceToVideo(element, src.dataset.mobileVid);
-            }
-        }
-
-        //init only if page has videos
-        function videoSize() {
-            //get all videos
-            var video = document.querySelectorAll('video.js-responsive-video')
-            if (video !== undefined) {
-            video.forEach(function(element, index) {
-                whichSizeVideo(
-                    element, //element
-                    element  //src locations
-                );
-            });
-            }
-        }
-        videoSize();
-
+    if (this.element.dataset[srcDatasetKey] === undefined) {
+      return;
     }
+
+    const source = document.createElement('source');
+    source.src = this.element.dataset[srcDatasetKey];
+    source.type = 'video/mp4';
+
+    this.element.appendChild(source);
+  }
 }
