0e8382c1a060ade51933a05138af397b900435a6
Petra Mišeta <petra.m@netgen.io>
2024-04-25 11:48:58 +0200

    NGSTACK-854 add listener to update bookmarks title on content update

---
+++ src/EventListener/Ibexa/Bookmarks/UpdateTitleListener.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener\Ibexa\Bookmarks;
+
+use App\Repository\BookmarkRepository;
+use Ibexa\Contracts\Core\Repository\Events\Content\UpdateContentEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+final class UpdateTitleListener implements EventSubscriberInterface
+{
+    public function __construct(private BookmarkRepository $bookmarkRepository) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            UpdateContentEvent::class => 'onUpdateContent',
+        ];
+    }
+
+    public function onUpdateContent(UpdateContentEvent $event): void
+    {
+        $content = $event->getContent();
+
+        $this->bookmarkRepository->updateBookmarkNames($content->id, (string) $content->getName());
+    }
+}
