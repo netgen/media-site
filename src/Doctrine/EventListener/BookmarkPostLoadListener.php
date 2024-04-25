6c9da22206fd607b89e37a78af465b12c275e7a2
Petra Mišeta <petra.m@netgen.io>
2024-04-25 12:08:20 +0200

    NGSTACK-854 move doctrine listeners to separate folder

--- src/Doctrine/EventListener/BookmarkPostLoadListener.php
+++ src/Doctrine/EventListener/BookmarkPostLoadListener.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\EventListener;
+namespace App\Doctrine\EventListener;
 
 use App\Entity\Bookmark;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
