0516411f16bcd668d28e752c980d07f2c0543b81
Robert Kaurin <robert@netgen.io>
2025-01-17 09:24:08 +0100

    NGAIPLAY-32 adjust chat controller response

--- src/Controller/Chat.php
+++ src/Controller/Chat.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+use function explode;
 use function flush;
 use function mb_strlen;
 use function ob_flush;
@@ -37,10 +38,24 @@ class Chat extends AbstractController
             ob_flush();
             flush();
 
-            for ($i = 0; $i < 10; ++$i) {
-                usleep(250000);
+            $data = '{"incremental":[{"items":[{"__typename":"TextMessageOutput","id":"ck-e3772f2f-22c4-4e31-a375-225313d0d1df","createdAt":"2025-01-13T10:31:09.776Z","role":"assistant","content":[]}],"path":["generateCopilotResponse","messages",0]}],"hasNext":true}';
+            echo "\r\n---\r\n";
+            echo "Content-Type: application/json; charset=utf-8\r\n";
+            echo 'Content-Length: ' . mb_strlen($data) . "\r\n";
+            echo "\r\n";
+            echo $data;
+            echo "\r\n";
 
-                $data = '{"incremental":[{"items":[{"__typename":"TextMessageOutput","id":"ck-55f792ec-e160-42ed-8411-30ce3507ff7d","createdAt":"2025-01-16T06:27:15.711Z","role":"assistant","content":[]}],"path":["generateCopilotResponse","messages",0]}],"hasNext":true}';
+            ob_flush();
+            flush();
+
+            $responseString = 'This is a fake response from chat backend, test test.';
+            $words = explode(' ', $responseString); // Split the string into words
+
+            for ($i = 0; $i < 10; ++$i) {
+                usleep(100000);
+
+                $data = '{"incremental":[{"items":["' . $words[$i] . ' "],"path":["generateCopilotResponse","messages",0,"content",' . $i . ']}],"hasNext":true}';
                 echo "---\r\n";
                 echo "Content-Type: application/json; charset=utf-8\r\n";
                 echo 'Content-Length: ' . mb_strlen($data) . "\r\n";
