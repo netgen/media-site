8a1c129f48a7f04eab468a62e5c57b8d3763ef85
Petar Španja <petar@spanja.info>
2025-01-20 16:15:07 +0100

    NGAIPLAY-7: use LLM endpoint

--- src/Controller/ProxyChat.php
+++ src/Controller/ProxyChat.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 use function flush;
+use function mb_strlen;
 use function ob_flush;
 use function usleep;
 
@@ -26,17 +27,77 @@ class ProxyChat extends AbstractController
         $response->headers->set('Content-Type', 'multipart/mixed; boundary="-"');
         $response->headers->set('X-Accel-Buffering', 'no');
 
-        $remoteResponse = $client->request('GET', 'https://netgen-ai-site.dev10.netgen.biz/ai');
+        $remoteResponse = $client->request(
+            'POST',
+            // send_message_to_rag
+            'http://192.168.10.219:8000/api/rag/send_message_to_llm',
+            [
+                // + session_id, filter_field
+                'body' => '{"query": "who is messi?"}',
+                'headers' => [
+                    'accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                ],
+            ],
+        );
 
         $response->setCallback(static function () use ($client, $remoteResponse) {
+            $data = '{"data":{"generateCopilotResponse":{"threadId":"ck-5eecb86e-2095-4769-88ea-73532be385a7","runId":null,"__typename":"CopilotResponse","messages":[]}},"hasNext":true}';
+            echo "\r\n---\r\n";
+            echo "Content-Type: application/json; charset=utf-8\r\n";
+            echo 'Content-Length: ' . mb_strlen($data) . "\r\n";
+            echo "\r\n";
+            echo $data;
+            echo "\r\n";
+
+            ob_flush();
+            flush();
+
+            $data = '{"incremental":[{"items":[{"__typename":"TextMessageOutput","id":"ck-e3772f2f-22c4-4e31-a375-225313d0d1df","createdAt":"2025-01-13T10:31:09.776Z","role":"assistant","content":[]}],"path":["generateCopilotResponse","messages",0]}],"hasNext":true}';
+            echo "\r\n---\r\n";
+            echo "Content-Type: application/json; charset=utf-8\r\n";
+            echo 'Content-Length: ' . mb_strlen($data) . "\r\n";
+            echo "\r\n";
+            echo $data;
+            echo "\r\n";
+
+            ob_flush();
+            flush();
+
+            $k = 1;
             foreach ($client->stream($remoteResponse) as $chunk) {
-                echo $chunk->getContent();
+                $payload = $chunk->getContent();
+                $data = '{"incremental":[{"items":["' . $payload . ' "],"path":["generateCopilotResponse","messages",0,"content",' . $k . ']}],"hasNext":true}';
+
+                echo "\r\n---\r\n";
+                echo "Content-Type: application/json; charset=utf-8\r\n";
+                echo 'Content-Length: ' . mb_strlen($data) . "\r\n";
+                echo "\r\n";
+                echo $data;
+                echo "\r\n";
 
                 ob_flush();
                 flush();
 
-                usleep(250000);
+                ++$k;
+                //                usleep(250000);
             }
+
+            $data = '{"hasNext":false}';
+            echo "---\r\n";
+            echo "Content-Type: application/json; charset=utf-8\r\n";
+            echo 'Content-Length: ' . mb_strlen($data) . "\r\n";
+            echo "\r\n";
+            echo $data;
+            echo "\r\n";
+
+            ob_flush();
+            flush();
+
+            echo "-----\r\n";
+
+            ob_flush();
+            flush();
         });
 
         return $response;
