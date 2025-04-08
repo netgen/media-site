b5178ff8baa1d154bd35e85f2caf8e7cf7caae27
Lora Žuliček <lora@netgen.io>
2025-04-08 15:33:18 +0200

    NGSTACK-929 form validation

--- assets/js/components/FormEmbed.component.js
+++ assets/js/components/FormEmbed.component.js
@@ -1,15 +1,23 @@
 import submit from '../utilities/form/submit';
+import validateForm from '../utilities/form/validate';
 import setupUpdateSelectedFilesList from '../utilities/form/setup-update-selected-files-list';
 
 export default class FormEmbed {
   constructor(form) {
     this.form = form;
-
     this.init();
   }
 
   init() {
     setupUpdateSelectedFilesList(this.form);
-    this.form.addEventListener('submit', submit.bind(this, 'embed'));
+    this.form.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    if (validateForm(this.form)) {
+      submit.bind(this, 'embed', e)();
+    }
   }
 }
